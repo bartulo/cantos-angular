a2a5f0605bba9f6847fafae994862ccfd7868752
bartulo <bartulo@gmail.com>
2020-05-31 15:55:14 +0200

    arreglado texto de softkey

--- src/app/components/softkey/softkey.component.ts
+++ src/app/components/softkey/softkey.component.ts
@@ -1,54 +1,16 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
-import {NavigationService} from 'src/app/services/navigation.service';
-import {Router, ActivatedRoute, Params} from '@angular/router';
-
-interface Softkey {
-  left: string;
-  center: string;
-  right: string;
-}
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-softkey',
   templateUrl: './softkey.component.html',
   styleUrls: ['./softkey.component.css']
 })
-export class SoftkeyComponent implements OnInit, AfterViewChecked{
-  private homeSoftkey: Softkey = { left: '', center:'Enter', right: 'Buscar' };
-  private searchSoftkey: Softkey = { left: '', center:'Volver', right: 'Limpiar' };
-  private songsSoftkey: Softkey = { left: 'Volver', center:'Enter', right: '' };
-  private playerSoftkey: Softkey = { left: 'Inicio', center:'Pause', right: 'Volver' };
-  public softkey: Softkey = this.homeSoftkey;
-  public page: string;
+export class SoftkeyComponent {
 
-  constructor(private navigationService: NavigationService, private router: Router, private route: ActivatedRoute) {
+  @Input() right: string;
+  @Input() center: string;
+  @Input() left: string;
+
+  constructor() {
   }
-
-  ngOnInit(){
-    this.route.params.subscribe((params: Params) => { 
-      console.log(params);
-    });
-  }
-
-  ngAfterViewChecked() {
-    this.toHearChangesNavigation();
-  }
-
-  private toHearChangesNavigation(): void {
-    this.navigationService.currentItem.subscribe(current =>{
-      this.page = this.router.url.split('/')[1];
-      if (this.page == 'songsPlayer') {
-        this.softkey = this.playerSoftkey;
-      } else if (this.page == 'songsList') {
-        this.softkey = this.songsSoftkey;
-      } else {
-        if (current.nodeName == 'INPUT') {
-          this.softkey = this.searchSoftkey;
-        } else {
-          this.softkey = this.homeSoftkey;
-        }
-      }
-    });
-  }
-
 }
